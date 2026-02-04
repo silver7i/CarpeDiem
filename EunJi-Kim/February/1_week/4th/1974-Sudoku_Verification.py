1c337f24e3f9f0398e5bab5ccd2e6ffa4ed2f8fa
ejkim <silverland0417@gmail.com>
2026-02-05 00:21:50 +0900

    어디에 단어 문제 풀던 중

--- EunJi-Kim/February/1_week/4th/1974-Sudoku_Verification.py
+++ EunJi-Kim/February/1_week/4th/1974-Sudoku_Verification.py
@@ -10,3 +10,31 @@ sys.stdin = open(file_path, "r")
 
 
 ###############################
+
+T = int(input())
+for test_case in range(1, T + 1):
+    N = 9
+    arr = [list(map(int, input().split())) for _ in range(N)]
+
+    is_correct = 1
+    # 세트의 길이가 N이면 1, 아니면 0
+    for i in range(N):
+        col_set = set()
+        row_set = set()
+        for j in range(N):
+            row_set.add(arr[i][j])
+            col_set.add(arr[j][i])
+
+        if len(row_set) != N or len(col_set) != N:
+            is_correct = 0
+            break
+
+    for i in range(0, 9, 3):
+        rec_set = set()
+        for j in range(3):
+            for k in range(3):
+                rec_set.add(arr[j][k])
+        if len(rec_set) != N:
+            is_correct = 0
+
+    print(f'#{test_case} {is_correct}')
