9322097292dd83ae1c5938913dc5b08a8b99d03a
ejkim <silverland0417@gmail.com>
2026-02-07 16:26:31 +0900

    feat & docs : Solved 3 string algorithm problems

--- EunJi-Kim/February/1_week/6th/1989-palindrome_test.py
+++ EunJi-Kim/February/1_week/6th/1989-palindrome_test.py
@@ -10,6 +10,15 @@ sys.stdin = open(file_path, "r")
 
 ###############################
 
+def is_palindrome(word):
+    for i in range(len(word)//2):
+        if word[i] != word[-1-i]:
+            return 0
+    return 1
+            
+
 T = int(input())
 for test_case in range(1, T + 1):
-    pass
+    word = input().strip()
+
+    print(f'#{test_case} {is_palindrome(word)}')
