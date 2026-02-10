dec279817220a20488bdf381e0b861808571e292
jisu <jisudori0402@naver.com>
2026-02-11 08:29:22 +0900

    2026-02-10

---
+++ JiSu_Moon/Feb/2_week/10th/5201.py
@@ -0,0 +1,24 @@
+T = int(input())
+for tc in range(1, T+1):
+    N, M = map(int, input().split())
+    W = list(map(int,input().split()))
+    Z = list(map(int,input().split()))
+
+    # 내림차순 정렬
+    w = sorted(W, reverse=True)
+    z = sorted(Z, reverse=True)
+    total = 0
+    st = 0
+
+    for i in range(M):  # 트럭 수
+        flag = 0
+        for j in range(st, N):  # 컨테이너 수, 옮긴 다음 화물부터
+            if z[i] >= w[j]:
+                flag = 1
+                st = j+1
+                total += w[j]
+                break
+        if flag == 0:
+            break
+
+    print(f'#{tc} {total}')
---
+++ JiSu_Moon/Feb/2_week/10th/9489.py
@@ -0,0 +1,35 @@
+T = int(input())
+for tc in range(1, T+1):
+    N, M = map(int, input().split())
+    arr = [list(map(int, input().split())) for _ in range(N)]
+    Max = -21e8
+
+    def count(i,j):
+        count = 0
+        row = 0  # 가로
+        for x in range(M):
+            if j+x<M and arr[i][j+x] == 1:
+                row += 1
+            else:
+                break
+        columm = 0  # 세로
+        for x in range(N):
+            if i+x<N and arr[i+x][j] == 1:
+                columm += 1
+            else:
+                break
+        if row > columm:
+            count = row
+        else:
+            count = columm
+        return count
+
+
+    for i in range(N):  # 행
+        for j in range(M):  # 열
+            if arr[i][j] == 1:  # 시작
+                result = count(i,j)
+                if result > Max:
+                    Max = result
+
+    print(f'#{tc} {Max}')
