7de64c7db81006fe4c852a3d46174df726be2117
hwarang <asdf78964@naver.com>
2026-02-10 17:56:09 +0900

    멋쟁이 토마토 토마토git add .!

---
+++ Hwa_Rang/Febuary/2week/Java_02_10/src/SWEA_1230_암호문3.java
@@ -0,0 +1,58 @@
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+public class SWEA_1230_암호문3 {
+public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		for(int tc=1; tc<=10; tc++) {
+			int n = sc.nextInt();
+			
+			List<Integer> list = new LinkedList<>();
+			
+			for(int i=0; i<n; i++) {
+				list.add(sc.nextInt());
+			}
+			
+			int k = sc.nextInt();
+			
+			for(int i=0; i<k; i++) {
+				String s = sc.next();
+
+				
+				if(s.equals("I")) {
+					int start = sc.nextInt();
+					int last = sc.nextInt();
+					for(int j=0; j<last; j++) {
+						list.add(start+j, sc.nextInt());
+					}
+				}
+				else if(s.equals("D")) {
+					int start = sc.nextInt();
+					int last = sc.nextInt();
+					for(int j=0; j<last; j++) {
+						list.remove(start+j);
+					}
+				}
+				else {
+					int last = sc.nextInt();
+					for(int j=0; j<last; j++) {
+						list.add(sc.nextInt());
+					}
+				}		
+				
+				
+			}
+			int cnt = 0;
+			System.out.print("#"+tc+" ");
+			for(Integer elem : list) {
+				System.out.print(elem+" ");
+				cnt++;
+				if(cnt == 10) break;
+			}
+			System.out.println();
+		}
+	}
+}
