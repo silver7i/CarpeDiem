274a97243d79abf7f119fd386515e42cfa2f2503
hwarange <asdf78964@naver.com>
2026-02-14 21:20:09 +0900

    README 추가

--- Hwa_Rang/Febuary/2week/Java_02_13/src/SWEA_1979_어디에단어가들어갈수있을까.java
+++ Hwa_Rang/Febuary/2week/Java_02_13/src/SWEA_1979_어디에단어가들어갈수있을까.java
@@ -4,78 +4,149 @@ public class SWEA_1979_어디에단어가들어갈수있을까 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		int n = sc.nextInt();
-		int k = sc.nextInt();
+		int tc = sc.nextInt();		
 		
-		int[][] map = new int[n][n];
-		
-		for(int i=0; i<n; i++) {
-			for(int j=0; j<n; j++) {
-				map[i][j] = sc.nextInt();
-			}
-		}
-		
-		int cnt = 0;
-		
-		if(k == n) {
-			for(int i=0; i<n; i++) {
-				int sum = 0;
-				for(int j=0; j<n; j++) {
-					if(map[i][j] == 1) sum++;
-					else break;
-				}
-				
-				if(sum == k) cnt++;
-			}
-		}
-		
-		else {
+		for(int testcase = 1; testcase<=tc; testcase++) {
+			
+			int n = sc.nextInt();
+			int k = sc.nextInt();
+			
+			
+			int[][] map = new int[n][n];
 			
 			for(int i=0; i<n; i++) {
 				for(int j=0; j<n; j++) {
+					map[i][j] = sc.nextInt();
+				}
+			}
+			
+			int cnt = 0;
+			
+			if(k == n) {
+				
+				//가로열 확인
+				for(int i=0; i<n; i++) {
 					int sum = 0;
-					if(j==0) {
-						for(int t=j; t<=k; t++) {
-							if(map[i][t] == 1) sum ++;
-							else break;
-						}
-					}
-					
-					else if(j+k == n) {
-						for(int t=j-1; t<=k+j-1; t++) {
-							if(t == j-1 && map[i][t] == 1) {
-								System.out.println("he");
-								break;
-							}
-							
-							if(map[i][t] == 1) {
-								sum++;
-							}
-							else break;
-						}
-					}
-					
-					else {
-						for(int t=j-1; t<k+j; t++) {
-							if(t == j-1 && map[i][t] == 1) break;
-							
-							if(map[i][t] == 1) {
-								System.out.println("hellp");
-								sum++;
-							}
-							else break;
-						}
+					for(int j=0; j<n; j++) {
+						if(map[i][j] == 1) sum++;
+						else break;
 					}
 					
 					if(sum == k) cnt++;
 				}
 				
+				
+				//세로열 확인
+				for(int j=0; j<n; j++) {
+					int sum = 0;
+					for(int i=0; i<n; i++) {
+						if(map[i][j] == 1) sum++;
+						else break;
+					}
+					
+					if(sum == k) cnt++;
+				}
 			}
 			
+			else {
+				
+				//가로열 확인
+				for(int i=0; i<n; i++) {
+					for(int j=0; j<n; j++) {
+						int sum = 0;
+						if(j==0) {
+							for(int t=j; t<=k; t++) {
+								if(map[i][t] == 1) sum ++;
+								else break;
+							}
+						}
+						
+						else if(j == n-k) {
+							for(int t=j; t<k+j; t++) {
+								if(map[i][j-1] == 1 ) {
+									break; 
+								}
+								
+								if(map[i][t] == 1) {
+									sum++;
+								}
+								
+								else break;
+							}
+						}
+						
+						else {
+							for(int t=j-1; t<k+j; t++) {
+								if(t == j-1 && map[i][t] == 1) break;
+								
+								if(map[i][t] == 1) {
+									sum++;
+								}
+								else break;
+							}
+						}
+						
+						if(sum == k) cnt++;
+					}
+					
+				}
+				
+				//세로열 확인
+				
+				for(int i=0; i<n; i++) {
+					for(int j=0; j<n; j++) {
+						int sum = 0;
+						if(j==0) {
+							for(int t=j; t<=k; t++) {
+								if(map[t][i] == 1) {
+									sum ++;
+									System.out.println("1");
+								}	
+								else break;
+							}
+						}
+						
+						else if(j == n-k) {
+							System.out.println("2");
+							for(int t=j; t<k+j; t++) {
+								if(map[j-1][i] == 1 ) {
+									break; 
+								}
+								
+								if(map[t][i] == 1) {
+									sum++;
+								}
+								
+								else break;
+							}
+						}
+						
+						else {
+							System.out.println("3");
+							for(int t=j-1; t<k+j; t++) {
+								if(t == j-1 && map[t][i] == 1) break;
+								
+								if(map[t][i] == 1) {
+									sum++;
+								}
+								else break;
+							}
+						}
+						
+						if(sum == k) cnt++;
+					}
+					
+				}
+				
+				
+				
+			}
+			
+			System.out.println("#"+testcase+" "+cnt);
 			
 			
 		}
 		
-		System.out.println(cnt);
+		
 	}
 }
