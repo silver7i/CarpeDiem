3266d789667a0f4ac42ce273fd8d5d044003fd45
ejkim <silverland0417@gmail.com>
2026-02-18 00:21:40 +0900

    숫자 만들기 AI 도움받아서 해결

--- EunJi-Kim/February/3_week/16th-18th_holiday/s4008-make_num.py
+++ EunJi-Kim/February/3_week/16th-18th_holiday/s4008-make_num.py
@@ -10,49 +10,36 @@ sys.stdin = open(file_path, "r")
 
 ###############################
 
-def check(op_arr, op_index, nums, nums_index, min_max):
-    if ' ' not in nums:
-        left = int(nums[0])
-        for i in range(len(nums)):
-            if nums[i] == '+':
-                left += int(nums[i+1])
-            elif nums[i] == '-':
-                left -= int(nums[i+1])
-            elif nums[i] == '*':
-                left *= int(nums[i+1])
-            elif nums[i] == '/':
-                left //= int(nums[i+1])
-        min_max[0] = min(min_max[0], left)
-        min_max[0] = max(min_max[1], left)
+def check(nums, nums_idx, now_value, plus, minus, mul, div, min_max):
+    if nums_idx == len(nums):
+        min_max[0] = min(min_max[0], now_value)
+        min_max[1] = max(min_max[1], now_value)
         return True
-
-    for i in range(op_index, len(op_arr)):
-        for j in range(i, len(op_arr)):
-            nums[nums_index] = op_arr[j]    # 연산자 넣기
-            # 만약 연산자가 다 채워졌다면 
-            if check(op_arr, i+1, nums, nums_index+2, min_max):
-                nums[nums_index] = ' '
     
-    return True
-                
-                
+    if plus > 0:
+        check(nums, nums_idx+1, now_value + nums[nums_idx], plus - 1, minus, mul, div, min_max)
+    if minus > 0:
+        check(nums, nums_idx+1, now_value - nums[nums_idx], plus, minus-1, mul, div, min_max)
+    if mul > 0:
+        check(nums, nums_idx+1, now_value * nums[nums_idx], plus, minus, mul-1, div, min_max)
+    if div > 0:
+        check(nums, nums_idx+1, int(now_value / nums[nums_idx]), plus, minus, mul, div-1, min_max)
 
+    return False
+                
+            
 
 T = int(input())
 for tc in range(1, T+1):
     N = int(input())
     'operator_nums = 각 연산자의 개수 +, -, *, /'
     operator_nums = list(map(int, input().split()))
-    '숫자, ' ', 숫자, ' ' 형태로 들어오도록'
-    nums = list(map(str, input().strip()))
-
-    operator_arr = ['+'] * operator_nums[0]
-    operator_arr += ['-'] * operator_nums[1]
-    operator_arr += ['*'] * operator_nums[2]
-    operator_arr += ['/'] * operator_nums[3]
+    nums = list(map(int, input().split()))
     
-    min_max = [10000, 0]
+    min_max = [100000, -100000]
     
-    check(operator_arr, 0, nums, 1, min_max)
-    print(min_max)
+    # 연산자 자리가 다 채워질 동안 재귀하면서 nums 인덱스가 연산자자리랑 같으면 minmax 계산
+  
+    check(nums, 1, nums[0], operator_nums[0], operator_nums[1], operator_nums[2], operator_nums[3], min_max)
+    print(f'#{tc} {min_max[1]-min_max[0]}')
     
