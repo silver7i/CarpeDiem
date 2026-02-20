1ccf4bd63f8a59f712a05456850381c675601c5e
ejkim <silverland0417@gmail.com>
2026-02-20 22:47:00 +0900

    토너먼트 카드게임 풀이 중...

--- EunJi-Kim/February/2_week/12th/4880-tournament_card_game.py
+++ EunJi-Kim/February/2_week/12th/4880-tournament_card_game.py
@@ -9,7 +9,47 @@ file_path.touch(exist_ok=True)  # 파일이 없으면 생성
 sys.stdin = open(file_path, "r")
 
 ###############################
+'''
+1-가위 / 2-바위 / 3-보
+A-B = 
+1 2  -1 => B
+1 3  -2 => A
+2 1   1 => A
+2 3  -1 => B
+3 1   2 => B
+3 2   1 => A
+A(-2, 1)
+B(-1, 2)
+'''
 
-T = 10
+def tournament(left_arr, right_arr):
+    # 만약 1:1 상황이 됐으면 가위바위보 결과 도출
+    if len(left_arr) == 1 and len(right_arr) == 1:
+        left = left_arr.pop()
+        right = right_arr.pop()
+        if left == right:
+            return left
+        elif left - right in [-2, 1]:
+            return left
+        else:
+            return right
+    elif len(left_arr) == 1 and len(right_arr) != 1:
+        return left_arr.pop()
+    
+    # 1:1이 안나왔으면 배열을 돌리면서 1:1이 되도록 설정
+    while len(left_arr) > 1:
+        if len(left_arr) >= 2:
+            left_arr.append(tournament(left_arr[:len(left_arr)//2], left_arr[len(left_arr)//2:]))
+        if len(right_arr) >= 2:
+            right_arr.append(tournament(right_arr[:len(right_arr)//2], right_arr[len(right_arr)//2:]))
+        
+        if len(left_arr) == 1 and len(right_arr) == 0:
+            return left_arr.pop()
+
+T = int(input())
 for test_case in range(1, T+1):  
-    pass
+    N = int(input())
+    arr = list(map(int, input().split()))
+
+    result = tournament(arr[:len(arr)//2], arr[len(arr)//2:])
+    print(result)
