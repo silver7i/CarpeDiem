2f4b78fbc53ff876d6a117e5deefe552e5d901a8
ejkim <silverland0417@gmail.com>
2026-02-20 22:45:43 +0900

    19일에 배열 최소 합 풀었음

--- EunJi-Kim/February/2_week/12th/4881-arr_minSum.py
+++ EunJi-Kim/February/2_week/12th/4881-arr_minSum.py
@@ -10,27 +10,32 @@ sys.stdin = open(file_path, "r")
 
 ###############################
 
-def find_min_sum(arr, N, now_i, now_j, select_ij, min_sum):
-    """
-    :param arr: N*N 배열
-    :param N: 고를 개수
-    :param now_ij: 지금의 행,열 1차원 배열
-    :param select_ij: 선택한 행, 열 2차원 배열
-    :param min_sum: 최소 합
-    """
-    
+def find_min_sum(arr, i, now_sum, visited):
+    global min_sum
 
-   
-    
+    if now_sum > min_sum:
+        return
+
+    if i == len(arr):
+        min_sum = min(min_sum, now_sum)
+        return
+
+    for j in range(len(arr[i])):
+        if not visited[j]:
+            now_sum += arr[i][j]
+            visited[j] = True
+            find_min_sum(arr, i+1, now_sum, visited)
+            now_sum -= arr[i][j]
+            visited[j] = False
 
 T = int(input())
 for test_case in range(1, T+1):  
     N = int(input())
     arr = [ list(map(int, input().split())) for _ in range(N)]
     
-    print(find_min_sum(arr, N, 0, 0, [], 100))
+    visited = [False] * N
+    min_sum = 100000
+    now_sum = 0
+    find_min_sum(arr, 0, now_sum, visited)
 
-    # 배열을 넘겨주고
-    # 행 인덱스랑 열 인덱스 저장하는 배열 필요할거 같고
-    # 지금 위치 필요할거 같고
-    # 고를 개수 N
+    print(f'#{test_case}', min_sum)
