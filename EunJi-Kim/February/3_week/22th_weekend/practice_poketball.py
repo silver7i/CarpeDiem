3cfe8a293443f223ddbbd7f5a48c19d9399dc719
ejkim <silverland0417@gmail.com>
2026-02-23 20:15:41 +0900

    modify file name

--- EunJi-Kim/February/3_week/22th_weekend/practice_poketball.py
+++ EunJi-Kim/February/3_week/22th_weekend/practice_poketball.py
@@ -4,19 +4,19 @@ from pprint import pprint
 ground = [[0] * 21 for _ in range(51)]
 
 holes = [[0,0], [0, 20], [50, 20], [50, 0]]
-target = [40, 10]
+target_ball = [40, 10]
 my_ball = [20, 5]
 
 for x, y in holes:
     ground[x][y] = 3
 
-ground[target[0]][target[1]] = 2
+ground[target_ball[0]][target_ball[1]] = 2
 ground[my_ball[0]][my_ball[1]] = 1
 
 
 ### 삼각함수
-a = abs(target[0]-my_ball[0])
-b = abs(target[1]-my_ball[1])
+a = abs(target_ball[0]-my_ball[0])
+b = abs(target_ball[1]-my_ball[1])
 
 r = math.sqrt(a**2 + b**2)
 radian = math.atan(b/a)
@@ -33,6 +33,12 @@ def calculate_theta(x1, y1, x2, y2):
     print(a, b, theta)
     return theta
 
-calculate_theta(my_ball[0], my_ball[1], target[0], target[1])
+calculate_theta(my_ball[0], my_ball[1], target_ball[0], target_ball[1])
+
+### 접점으로 공 보내기
+
+
+
+
 
 pprint(ground)
