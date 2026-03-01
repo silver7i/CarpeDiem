c6357eb9f905a7518580f1df3d761ca6cc654037
ejkim <silverland0417@gmail.com>
2026-03-01 18:07:12 +0900

    백준 4963 섬의 개수 다양한 방법으로 푸는 중

---
+++ EunJi-Kim/February/4_week/28th-29th-weekend/b4963-island_num.py
@@ -0,0 +1,110 @@
+import sys
+from pathlib import Path
+from pprint import pprint
+
+# 1. 파일 경로 설정 및 폴더/파일 자동 생성
+file_path = Path(__file__).resolve().parents[3] / "input_src" / f"{Path(__file__).stem}.txt"
+file_path.touch(exist_ok=True)  # 파일이 없으면 생성
+
+# 2. 파일 읽기 연동
+sys.stdin = open(file_path, "r")
+
+###############################
+
+#1. stack 없이 재귀함수 방법
+'''
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10000)
+
+deltas = [
+    [0, 1],     # 동
+    [1, 1],     # 동남
+    [1, 0],     # 남
+    [1, -1],    # 남서
+    [0, -1],    # 서
+    [-1, -1],   # 서북
+    [-1, 0],    # 북
+    [-1, 1],    # 북동
+]
+
+def dfs(w, h, i, j):
+    for k in range(8):
+        ni, nj = i + deltas[k][0], j + deltas[k][1]
+        
+        if 0<=ni<h and 0<=nj<w and not visited[ni][nj]:
+            if arr[ni][nj] == 1:
+                visited[ni][nj] = True
+                dfs(w, h, ni, nj)
+
+
+while 1:
+    w, h = map(int, input().split())
+    if w == 0 and h == 0:
+        break
+
+    arr = [list(map(int, input().split())) for _ in range(h)]
+    
+    # 현재 위치 기준으로 모든 방향을 둘러보면서
+    # False로 생성한 visited에 체크
+        # False이면 방향 체크하고, True면 체크 안함
+    
+    visited = [[False] * w for _ in range(h)]
+    cnt = 0
+
+    for i in range(h):
+        for j in range(w):
+            if arr[i][j] == 1 and not visited[i][j]:
+                visited[i][j] = True
+                dfs(w, h, i, j)
+                cnt += 1
+
+    print(cnt)
+'''
+
+
+import sys
+input = sys.stdin.readline
+
+deltas = [
+    [0, 1],     # 동
+    [1, 1],     # 동남
+    [1, 0],     # 남
+    [1, -1],    # 남서
+    [0, -1],    # 서
+    [-1, -1],   # 서북
+    [-1, 0],    # 북
+    [-1, 1],    # 북동
+]
+
+def dfs(i, j):
+    while stack:
+        for k in range(8):
+            ni = i + deltas[k][0]
+            nj = j + deltas[k][1]
+            if 0<=ni<h and 0<=nj<w and not visited[ni][nj]:
+                if arr[ni][nj] == 1:
+                    stack.append([ni, nj])
+                    visited[ni][nj] = True
+        
+        now_i, now_j = stack.pop()
+        dfs(now_i, now_j)
+        
+
+
+while 1:
+    w, h = map(int, input().split())
+    if w == 0 and h == 0:
+        break
+
+    arr = [list(map(int, input().split())) for _ in range(h)]
+    
+    visited = [[False] * w for _ in range(h)]
+    stack= []
+
+    for i in range(h):
+        for j in range(w):
+            if arr[i][j] == 1 and not visited[i][j]:
+                visited[i][j] = True
+                stack.append([i, j])
+                dfs(i, j)
