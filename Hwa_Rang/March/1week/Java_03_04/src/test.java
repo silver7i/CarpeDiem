2a02aaf3a4923e4f716f701e3bd1d9ed849a1a10
ejkim <silverland0417@gmail.com>
2026-03-24 11:12:12 +0900

    타인 폴더 삭제

--- Hwa_Rang/March/1week/Java_03_04/src/test.java
+++
@@ -1,9 +0,0 @@
-
-public class test {
-	public static void main(String[] args) {
-		long sqrt = (long)Math.sqrt(4); //1
-		
-		
-		System.out.println(sqrt*sqrt);
-	}
-}
--- Hwa_Rang/March/2week/Java_03_11/src/SWEA_2105_디저트카페.java
+++
@@ -1,6 +0,0 @@
-
-public class SWEA_2105_디저트카페 {
-	public static void main(String[] args) {
-		
-	}
-}
